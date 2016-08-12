0713093ae3eea58add434bb20e17bab8564b5a13
tcrivelatti <tcrivelatti@WS-LAB01_149.univel.univel.br>
2016-08-11 21:50:30 -0300

    #3 Criação das entidades Venda e Entrega.

---
+++ src/main/java/br/univel/entity/Entrega.java
@@ -0,0 +1,26 @@
+package br.univel.entity;
+
+/**
+ * @author tcrivelatti
+ * 11 de ago de 2016
+ */
+
+public class Entrega {
+
+	private int idEntrega;
+	private String endereco;
+
+	public int getIdEntrega() {
+		return idEntrega;
+	}
+	public void setIdEntrega(int idEntrega) {
+		this.idEntrega = idEntrega;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
---
+++ src/main/java/br/univel/entity/Venda.java
@@ -0,0 +1,28 @@
+package br.univel.entity;
+
+import java.util.List;
+
+/**
+ * @author tcrivelatti
+ * 11 de ago de 2016
+ */
+
+public class Venda {
+
+	private int idVenda;
+	private List<String> itens;
+
+	public int getIdVenda() {
+		return idVenda;
+	}
+	public void setIdVenda(int idVenda) {
+		this.idVenda = idVenda;
+	}
+	public List<String> getItens() {
+		return itens;
+	}
+	public void setItens(List<String> itens) {
+		this.itens = itens;
+	}
+
+}
