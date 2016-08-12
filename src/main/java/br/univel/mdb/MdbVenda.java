5b672ccd124124de451c210b59ffe6889a62597c
tcrivelatti <tcrivelatti@WS-LAB01_149.univel.univel.br>
2016-08-11 22:34:34 -0300

    #6 Criado 5 MDBs para escutar o tópico.

---
+++ src/main/java/br/univel/mdb/MdbVenda.java
@@ -0,0 +1,46 @@
+package br.univel.mdb;
+
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
+
+import br.univel.entity.Venda;
+
+/**
+ * @author tcrivelatti
+ * 11 de ago de 2016
+ */
+
+@MessageDriven(name = "MdbVenda", activationConfig = {
+	    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/TopicVenda"),
+	    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+	    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+	    @ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1")})
+
+public class MdbVenda implements MessageListener {
+
+	private final static Logger LOGGER = Logger.getLogger(MdbVenda.class.toString());
+
+	public void onMessage(Message rcvMessage) {
+
+		ObjectMessage msg = null;
+        try {
+        	 if (rcvMessage instanceof ObjectMessage) {
+                 msg = (ObjectMessage) rcvMessage;
+                 Venda venda = (Venda) msg.getObject();
+             } else {
+                 LOGGER.warning("Message of wrong type:" + rcvMessage.getClass().getName());
+             }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+
+	}
+
+}
