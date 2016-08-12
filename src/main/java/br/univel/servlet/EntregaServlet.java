6dad44206ad7ca6abdad827be8dcb37a0d809749
tcrivelatti <tcrivelatti@WS-LAB01_149.univel.univel.br>
2016-08-11 21:42:47 -0300

    #2 Criação dos servlets Venda e Entrega com método doGet implementado.

---
+++ src/main/java/br/univel/servlet/EntregaServlet.java
@@ -0,0 +1,23 @@
+package br.univel.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/entrega")
+public class EntregaServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.write("Entrega servlet.");
+	}
+
+}
